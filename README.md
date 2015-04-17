eed145dd564633da95cbc5490dc94118ac6835f0
Gabriel Barth-Maron <gabriel_barth-maron@alumni.brown.edu>
2015-04-16 20:29:23 -0400

    Added documentation to JNet.java and flushed out readme

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# Dependencies
+This is a set of Java bindings to the [Caffe](http://caffe.berkeleyvision.org/)
+deep learning framework.
 
-1. JNI
+# Prerequisites
 
-# Compilation
+1. Caffe - [installation instructions](http://caffe.berkeleyvision.org/installation.html)
+2. JNI - this is already installed on most Linux operating systems
 
-``make java``
+# Installation
+
+1. Download this repository and place it within your caffe home directory
+2. Run ``make java`` or ``make`` inside the newly downloaded directory
+
+This will generate a ``.jar`` and ``.so`` file in the ``lib/`` directory.
 
 # Usage
+In order to use the Java bindings you must:
+
+1. Add the ``caffe_jni.jar`` file to your project's dependencies
+2. Make sure ``/usr/local/cuda/lib64`` and ``$(CAFFE_HOME)/build/lib`` are both
+    in the ``LD_LIBRARY_PATH`` environment variable.
--- src/edu/h2r/JNet.java
+++ src/edu/h2r/JNet.java
@@ -16,11 +16,22 @@ public class JNet implements Disposable {
     private final float inputScale;
     private long internalPtr;
 
-    public JNet(String param_file, String pretrained_param_file, float inputScale) {
-        internalPtr = createNet(param_file, pretrained_param_file);
+    /**
+     * @param model_file the simplified model file (aka the deploy file)
+     * @param pretrained_param_file a trained .caffemodel file
+     * @param inputScale a float used to multiply inputs and scale them
+     */
+    public JNet(String model_file, String pretrained_param_file, float inputScale) {
+        internalPtr = createNet(model_file, pretrained_param_file);
         this.inputScale = inputScale;
     }
 
+    /**
+     * Performs a full forward pass through the network and returns its output.
+     * @param image the image to use as input
+     * @return the output of the network
+     * @throws IllegalArgumentException
+     */
     public float[] forward(BufferedImage image) throws IllegalArgumentException {
         if (image.getWidth() != getInputWidth() || image.getHeight() != getInputHeight())
             throw new IllegalArgumentException("Image does not have correct dimensions");
@@ -29,6 +40,14 @@ public class JNet implements Disposable {
         return forwardTo(imageToArray(image), layerNames[layerNames.length - 1]);
     }
 
+    /**
+     * Runs a forward pass through the network and returns the output at a specific layer.
+     * The forward pass is done only up to the specified layer.
+     * @param image the image to use as input
+     * @param toLayerName the layer whose output will be returned
+     * @return the output of layer toLayerName
+     * @throws IllegalArgumentException
+     */
     public float[] forwardTo(BufferedImage image, String toLayerName) throws IllegalArgumentException {
         if (image.getWidth() != getInputWidth() || image.getHeight() != getInputHeight())
             throw new IllegalArgumentException("Image does not have correct dimensions");
@@ -39,6 +58,11 @@ public class JNet implements Disposable {
         return forwardTo(imageToArray(image), toLayerName);
     }
 
+    /**
+     * Converts a {@link java.awt.image.BufferedImage} to a float array.
+     * @param image the image to convert
+     * @return a float array whose values are the pixel values of the image
+     */
     public static float[] imageToArray(BufferedImage image) {
         int w = image.getWidth();
         int h = image.getHeight();
@@ -48,6 +72,13 @@ public class JNet implements Disposable {
         return pixels;
     }
 
+    /**
+     * Creates a {@link java.awt.image.BufferedImage} from an array of pixels.
+     * @param pixels the pixels used to create the image
+     * @param width the image's width
+     * @param height the image's height
+     * @return a {@link java.awt.image.BufferedImage}
+     */
     public static BufferedImage imageFromArray(int[] pixels, int width, int height) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
         WritableRaster raster = image.getRaster();
@@ -55,6 +86,11 @@ public class JNet implements Disposable {
         return image;
     }
 
+    /**
+     * Converts gray-scale pixels to RGB values.
+     * @param grayPixels float array of gray-scale pixels
+     * @return
+     */
     public static int[] grayToRGB(float[] grayPixels) {
         int[] rgbPixels = new int[grayPixels.length];
         for (int i = 0; i < grayPixels.length; i++) {
@@ -67,6 +103,11 @@ public class JNet implements Disposable {
         return rgbPixels;
     }
 
+    /**
+     * Converts gray-scale pixels to RGB values.
+     * @param grayPixels integer array of gray-scale pixels
+     * @return
+     */
     public static int[] grayToRGB(int[] grayPixels) {
         int[] rgbPixels = new int[grayPixels.length];
         for (int i = 0; i < grayPixels.length; i++) {
@@ -78,24 +119,62 @@ public class JNet implements Disposable {
         return rgbPixels;
     }
 
-    // Methods implemented in C++
+    /**
+     * The following methods are implemented in C++.
+     * See edu_h2r_JNet.h and _net.cpp for their C++ definitions and implementations.
+     */
 
-    public native long createNet(String param_file, String pretrained_param_file);
+    /**
+     * Instantiates a Caffe neural network from the model_file and sets its weights using the pretrained_param_file.
+     * The {@link edu.h2r.JNet} class is a wrapper around the underlying Caffe neural network, so store a pointer to
+     * it in the internalPtr parameter.
+     * @param model_file a Caffe model definition file
+     * @param pretrained_param_file a .caffemodel file that contains weights for the Caffe model we are using
+     * @return a pointer to the underlying Caffe neural network
+     */
+    public native long createNet(String model_file, String pretrained_param_file);
 
+    /**
+     * Deletes the underlying Caffe neural network
+     */
     public native void dispose();
 
+    /**
+     * A wrapper around the Caffe neural network's forwardTo method.
+     * @param input the input data
+     * @param toLayerName the name of the layer whose output we wish to retrieve
+     * @return the output from the layer whose name is toLayerName
+     */
     public native float[] forwardTo(float[] input, String toLayerName);
 
+    /**
+     * @return the names of all the layers in the neural network
+     */
     public native String[] getLayerNames();
 
-    public native int getNodeCount(String layerName);  // TODO: What's this for?
+    /**
+     * Gets the number of nodes a layer has.
+     * @param layerName
+     * @return
+     */
+    public native int getNodeCount(String layerName);
 
+    /**
+     * @return the input height that the Caffe neural network is expecting.
+     */
+    public native int getInputHeight();
+
+    /**
+     * @return the input width that the Caffe neural network is expecting.
+     */
+    public native int getInputWidth();
+
+    /**
+     * Checks to see if the neural network has a layer whose name is layerName.
+     * @param layerName
+     * @return
+     */
     private native boolean hasLayer(String layerName);
-
-    private native int getInputHeight();
-
-    private native int getInputWidth();
-
 
     static {
         File jar = new File(JNet.class.getProtectionDomain().getCodeSource().getLocation().getPath());
